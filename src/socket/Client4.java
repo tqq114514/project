9c399b6dff12dc2a45b212ea0e8b1495543a40e9
tqq <1157595131@qq.com>
2023-03-16 11:02:08 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	out/production/project/ChatImprovePractice/Client.class
    #	out/production/project/ChatImprovePractice/Server$ClientHandler.class
    #	out/production/project/ChatImprovePractice/Server.class

--- src/socket/Client4.java
+++ src/socket/Client4.java
@@ -4,14 +4,33 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-
-public class Client3 {
+/**
+ * 聊天室的客户端
+ *
+ * V3版的客户端start方法中的主要逻辑是先等待用户在控制台上输入一行字符串,然后将其发送给服务端后再读取
+ * 来自服务端发送过来的消息.
+ * 但是由于V4服务器已经改成了可以将其他客户端的消息也转发给所有有客户端的操作,这会导致有源源不断的新消息
+ * 发送给客户端,然后如果客户端必须先等待用户输入一行再接收一行来自服务端消息的话,会导致接收消息不畅问题.
+ *
+ * 为了解决这个问题:
+ * 我们应当将发送消息和接收消息的操作分别在不同的线程上运行,才能做到互不干涉.
+ *
+ * 实现:
+ * 我们将发送消息还定义在start方法中由客户端的主线程执行即可,将读取消息单独定义到一个线程上执行
+ * 1:定义内部类ServerHandler,这是一个线程任务,主要工作就是读取来自服务端发送的消息并输出到客户端的
+ *   控制台上
+ * 2:在start方法一开始就启动一个线程来执行ServerHandler这个任务,不断接收来自服务端的消息.
+ *
+ *
+ *
+ */
+public class Client4 {
     private Socket socket;
-    public Client3(){
+    public Client4(){
         try {
             System.out.println("正在连接服务器");
-           /* socket = new Socket("176.17.9.23",8088);*/
-            socket = new Socket("localhost",23333);
+            socket = new Socket("176.17.9.23",8088);
+            /*socket = new Socket("localhost",23333);*/
             /*socket = new Socket("176.17.9.44",8088);*/
             System.out.println("与服务器连接成功");
         } catch (IOException e) {
@@ -20,16 +39,15 @@ public class Client3 {
     }
     public void start(){
         try {
+            /*启动用于接受服务器消息的线程*/
+            ServerHandler sh = new ServerHandler();
+            Thread thread = new Thread(sh);
+            thread.start();
+
             OutputStream os = socket.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
             BufferedWriter bw = new BufferedWriter(osw);
             PrintWriter pw = new PrintWriter(bw,true);
-
-            /*暂时接受服务端返回的消息，临时*/
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is,StandardCharsets.UTF_8);
-            BufferedReader br = new BufferedReader(isr);
-
             System.out.println("请按行输入要传输的消息：");
             Scanner scanner = new Scanner(System.in);
             while (true){
@@ -38,9 +56,6 @@ public class Client3 {
                     break;
                 }
                 pw.println(line);
-                /*给服务端发送内容的同时，接受服务端发送的内容，临时*/
-                line = br.readLine();
-                System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,7 +69,28 @@ public class Client3 {
     }
 
     public static void main(String[] args) {
-        Client3 client = new Client3();
+        Client4 client = new Client4();
         client.start();
+
+    }
+    private class ServerHandler implements Runnable{
+        /*该线程负责不断接受服务端转发来的信息*/
+        @Override
+        public void run() {
+            /*暂时接受服务端返回的消息，临时*/
+            try {
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is,StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+                /*给服务端发送内容的同时，接受服务端发送的内容，临时*/
+                String line;
+                while ((line = br.readLine())!=null){
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
---
+++ src/socket/Server5.java
@@ -0,0 +1,123 @@
+package socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+/*多线程版本，利用多线程完成与客户端交互，使得主线程只负责接受连接
+在server中定义内部类ClientHandler来实现Runnable接口，作为线程任务
+
+/**
+ * 聊天室的服务端 版本3
+ *
+ * 这个版本我们实现将消息发送给客户端
+ *
+ * 目前先实现将客户端发送过来的消息回复给该客户端
+ * 具体做法:
+ * 1:每个ClientHandler中,我们通过socket获取输出流,并进行流连接为PrintWriter
+ * 2:每当我们读取到来自客户端发送过来的一行字符串后,就将它通过PrintWriter再发送回给该客户端
+ * 通过这个测试,实现服务端给客户端发送消息的操作
+ *
+ */
+
+public class Server5 {
+    private ServerSocket serverSocket;
+    /*该集合用于保存所有客户端的输出流，并广播信息*/
+    private ArrayList<PrintWriter> allOut = new ArrayList<>();
+    public Server5(){
+        try {
+                System.out.println("启动服务端");
+                serverSocket = new ServerSocket(23333);
+                System.out.println("服务端启动完毕");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void start(){
+        System.out.println("等待客户端连接");
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("已建立一个连接");
+                /*启动一个线程与客户端交互*/
+                Runnable ra = new ClientHandler(socket);
+                Thread thread = new Thread(ra);
+                thread.start();
+            }
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    public static void main(String[] args) {
+        Server5 server = new Server5();
+        server.start();
+    }
+    private  class ClientHandler implements Runnable{
+
+        private final Socket socket;
+        private final String host; //记录已连接客户端的IP地址
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+            host = socket.getInetAddress().getHostAddress();
+        }
+        /*与客户端交互的线程任务*/
+        @Override
+        public void run() {
+              PrintWriter pw = null;  //由于作用域的问题，我们在finally里调用不到pw,需要在外面声明
+            try {
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+
+                /*通过socket获取输出流用于给客户端发送消息*/
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+                BufferedWriter bw = new BufferedWriter(osw);
+                pw = new PrintWriter(bw,true);
+
+                /*将对应该客户端的输出流存入共享集合allOut中，便于其他线程可以访问到*/
+                synchronized (allOut){
+                    allOut.add(pw);   //给allOut加锁，以防止线程抢占写入printWriter，导致数据异常
+                }
+                sendMessage(host+"上线了，当前聊天室在线人数为："+allOut.size());
+
+                String message;
+                while ((message = br.readLine()) != null) {
+                   sendMessage(host + "说：" + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                /*收尾工作，客户端下线操作*/
+                /*首先将该离线客户端的输出流在这个集合里面删掉*/
+                synchronized (allOut) {
+                    allOut.remove(pw);
+                }
+                /*广播这个人下线了*/
+                sendMessage(host+"下线了，当前在线人数："+allOut.size());
+                /*关闭socket,释放资源*/
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /*v5功能：实现广播消息的方法：
+        * 将消息转发给所有客户端
+        * */
+        private void sendMessage(String message){
+            System.out.println(host+ "客户端发到服务端的消息为：" + message);
+            /*遍历allOut,给所有客户端发消息*/
+            /*新循环是迭代器，迭代器要求遍历过程中不能用集合方法增删元素，所以需要和上面的增删互斥*/
+            for(PrintWriter out :allOut){
+                out.println(message);
+            }
+        }
+    }
+}
